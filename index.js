d81adab7e27b1e4fe6403130c4b1b22c9c26eb5b
Smold3r <71720593+Mulendeshi@users.noreply.github.com>
2024-09-23 15:23:25 +0200

    Commiting trial

---
+++ index.js
@@ -0,0 +1,11 @@
+var prod  = 8*12 ;
+let names = "Jackson";
+console.log("My  name is " + names + " I am " + prod + " Old");
+console.log(`Hello my name is ${names} and i am ${prod} years old`)
+
+function jac(a,b){
+b = a+b;
+return b;
+}
+
+console.log(jac("you", "hoo"));
